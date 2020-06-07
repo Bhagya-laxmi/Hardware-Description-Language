1169bf350a01ee0f0182982d92d89e73caeb29b7
Bhagya-laxmi <50071928+Bhagya-laxmi@users.noreply.github.com>
2020-06-07 22:44:42 +0200

    Update Sudoku_cell.vhd

--- SUDOKU/Sudoku_cell.vhd
+++ SUDOKU/Sudoku_cell.vhd
@@ -22,8 +22,14 @@ actual_comp:process(Reset,clk)
  variable cell_check: std_logic_vector(9 downto 1);
  variable temp_A,temp_B,temp_C,temp_D,temp_E,temp_F,temp_G,temp_H,temp_I: std_logic_vector(9 downto 1):="000000000";
  variable count_row,count_col,count_box: integer;
+ variable result_row, result_col,result_box, result_final: std_logic_vector(9 downto 1):="000000000";
+
 begin
 
+ result_row := "000000000";
+ result_col := "000000000";
+ result_box := "000000000";
+ result_final := "000000000";
  if( Reset = '1') then
   if( cell_init >0) then
    cell_check := Int_Vector(cell_init);
@@ -64,8 +70,62 @@ begin
   temp_F :=  temp_C and temp_E;
   temp_I := temp_F and temp_H;
   
-  cell_check := temp_I;
-  Cell_value <= temp_I;
+ loop1:for i in 9 downto 1 loop
+    if temp_I(i) = '1' then
+     for j in 7 downto 0 loop
+      if OneCount(row_values(j)) > 1 then
+       if row_values(j)(i) = '1' then
+        result_row(i) := '0';
+        next loop1;
+       else
+        result_row(i) := '1';
+       end if;
+      end if;
+     end loop;
+    end if;
+   end loop;
+
+
+loop2:for i in 9 downto 1 loop
+    if temp_I(i) = '1' then
+     for j in 7 downto 0 loop
+      if OneCount(column_values(j)) > 1 then
+       if column_values(j)(i) = '1' then
+        result_col(i) := '0';
+        next loop2;
+       else
+        result_col(i) := '1';
+       end if;
+      end if;
+     end loop;
+    end if;
+   end loop;   
+
+loop3:for i in 9 downto 1 loop
+    if temp_I(i) = '1' then
+     for j in 3 downto 0 loop
+      if OneCount(box_values(j)) > 1 then
+       if box_values(j)(i) = '1' then
+        result_box(i) := '0';
+        next loop3;
+       else
+        result_box(i) := '1';
+       end if;
+      end if;
+     end loop;
+    end if;
+   end loop;
+
+   result_final := result_row and result_col and result_box;
+  
+   if OneCount(result_final) = 1 then
+    cell_check := result_final;   
+   else
+    cell_check := temp_I;
+   end if;
+ 
+    Cell_value <= cell_check;
+ 
 end if;
 
 end process actual_comp;
